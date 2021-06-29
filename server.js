5c3a4b9280549d8f520d05889f6f441421fd54eb
unknown <formacio@PC-1062618.formacio.local>
2021-06-29 11:08:27 +0200

    add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require ('express');
 const app = express()
 
+
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+]
+app.get('/users', function(req,res){
+    res.json({
+        success: true,
+        message:' succesfully got users.',
+        users: mockUserData
+    })
+})
+
+
 app.listen(8000, function(){
     console.log("server is running")
 })
